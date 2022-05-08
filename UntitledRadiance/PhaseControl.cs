ffcb282ba7bc743c587b461bea0dd2357499241a
Njord Yu <yunjord@gmail.com>
2022-05-07 17:22:08 -0700

    Updated.

---
+++ UntitledRadiance/PhaseControl.cs
@@ -0,0 +1,68 @@
+﻿namespace UntitledRadiance;
+public partial class PhaseControl : Module
+{
+    public PhaseControl(UntitledRadiance untitledRadiance) : base(untitledRadiance)
+    {
+    }
+    public override List<(string, string)> GetPreloadNames()
+    {
+        return new List<(string, string)>
+        {
+        };
+    }
+    public override void LoadPrefabs(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+    {
+    }
+    public override void UpdateHitInstance(HealthManager healthManager, HitInstance hitInstance)
+    {
+    }
+    public override void UpdateFSM(PlayMakerFSM fsm)
+    {
+        if (IsAbsoluteRadiance(fsm.gameObject) && fsm.FsmName == "Phase Control")
+        {
+            fsm.AddCustomAction("Init", () =>
+            {
+                fsm.gameObject.GetComponent<HealthManager>().hp = 5000;
+                fsm.gameObject.RefreshHPBar();
+                fsm.AccessStringVariable("phase").Value = "1.1";
+            });
+            fsm.AddAction("Init", fsm.CreateGeneralAction(() =>
+            {
+                var hp = fsm.gameObject.GetComponent<HealthManager>().hp;
+                var phase = fsm.AccessStringVariable("phase").Value;
+                var spikeControl = fsm.gameObject.transform.parent.Find("Spike Control").gameObject;
+                if (phase == "1.1")
+                {
+                    if (hp <= 5000 - 32)
+                    {
+                        fsm.AccessStringVariable("phase").Value = "1.2";
+                        phase = fsm.AccessStringVariable("phase").Value;
+                        Log("Switching phase to: " + phase.ToString());
+                        spikeControl.LocateMyFSM("Control").SendEvent("SPIKE WAVES");
+                        Log("Switched phase to: " + phase.ToString());
+                    }
+                }
+                else if (phase == "1.2")
+                {
+                    if (hp <= 5000 - 32 * 2)
+                    {
+                        fsm.AccessStringVariable("phase").Value = "1.3";
+                        phase = fsm.AccessStringVariable("phase").Value;
+                        Log("Switching phase to: " + phase.ToString());
+                        spikeControl.LocateMyFSM("Control").SendEvent("SPIKE WAVES FULL");
+                        Log("Switched phase to: " + phase.ToString());
+                    }
+                }
+                else if (phase == "1.3")
+                {
+
+                }
+                else
+                {
+                    Log("Unknown phase: " + phase.ToString());
+                }
+            }));
+            fsm.RemoveTransition("Init", "FINISHED");
+        }
+    }
+}
---
+++ UntitledRadiance/SpikeControl.cs
@@ -0,0 +1,52 @@
+﻿namespace UntitledRadiance;
+public partial class SpikeControl : Module
+{
+    public SpikeControl(UntitledRadiance untitledRadiance) : base(untitledRadiance)
+    {
+    }
+    public override List<(string, string)> GetPreloadNames()
+    {
+        return new List<(string, string)>
+        {
+        };
+    }
+    public override void LoadPrefabs(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+    {
+    }
+    public override void UpdateHitInstance(HealthManager healthManager, HitInstance hitInstance)
+    {
+    }
+    public override void UpdateFSM(PlayMakerFSM fsm)
+    {
+        var gameObject = fsm.gameObject;
+        if (gameObject.scene.name == "GG_Radiance" && gameObject.name == "Spike Control" && fsm.FsmName == "Control")
+        {
+            fsm.AddState("Spike Waves Full");
+            fsm.AddGlobalTransition("SPIKE WAVES FULL", "Spike Waves Full");
+            fsm.AddCustomAction("Spike Waves Full", () =>
+            {
+                Log("Starting skill: Spike Waves Full");
+                var beamSweeper = (fsm.GetState("Wave L").Actions[0] as SetFsmBool).gameObject.GameObject.Value;
+                beamSweeper.LocateMyFSM("Control").AccessBoolVariable("Force Left").Value = false;
+                beamSweeper.LocateMyFSM("Control").AccessBoolVariable("Force Right").Value = false;
+                for (int i = 2; i <= 6; ++i)
+                {
+                    var spikes = (fsm.GetState("Wave L").Actions[i] as SendEventByName).eventTarget.gameObject.GameObject.Value;
+                    foreach (var fsm_ in spikes.GetComponentsInChildren<PlayMakerFSM>())
+                    {
+                        fsm_.SendEvent("UP");
+                    }
+                }
+                Log("Started skill: Spike Waves Full");
+            });
+        }
+        else if (gameObject.scene.name == "GG_Radiance" && gameObject.name.StartsWith("Radiant Spike") && fsm.FsmName == "Hero Saver")
+        {
+            fsm.InsertCustomAction("Send", () =>
+            {
+                HeroController.instance.damageMode = GlobalEnums.DamageMode.FULL_DAMAGE;
+                fsm.SendEvent("FINISHED");
+            }, 0);
+        }
+    }
+}
