bb268651bf09d37cff791bd805c633383ae06adb
Njord Yu <yunjord@gmail.com>
2022-05-21 17:20:16 -0700

    Updated.

--- UntitledRadiance/RotatingBurstRotator.cs
+++ UntitledRadiance/RotatingBurstRotator.cs
@@ -1,7 +1,7 @@
 ﻿namespace UntitledRadiance;
 public class RotatingBurstRotator : MonoBehaviour
 {
-    public float degreesPerSecond = 15;
+    public float degreesPerSecond = 10;
     private void Update()
     {
         gameObject.transform.Rotate(0, 0, Time.deltaTime * degreesPerSecond);
